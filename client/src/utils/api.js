1a0f589f8343eea6dc1a9b34f4b06b449c157f6e
Piyush Kumar <kumarchspiyush@gmail.com>
2024-10-07 22:20:47 +0530

    jhj

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "https://personal-blog-bo87.onrender.com"
+export const BASE_URL = "http://localhost:3000"
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://personal-blog-bo87.onrender.com',
+        target: 'http://localhost:3000',
         secure: false,
       },
     },
