3b24cde642454d0b5718c63b550af7f3abf740ed
Piyush Kumar <kumarchspiyush@gmail.com>
2024-10-15 01:17:37 +0530

    huge

--- server/index.js
+++ server/index.js
@@ -1,46 +1,20 @@
 import express from "express";
+const app = express();
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors';
-import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
-import postRoutes from "./routes/post.routes.js";
+import userRoutes from "./routes/user.routes.js"; // authRoutes
+import postRoutes from "./routes/post.routes.js"
 import commentRoutes from './routes/comment.routes.js';
 import cookieParser from 'cookie-parser';
+app.use(express.json());
 import path from 'path';
 
+
 dotenv.config();
+// app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
-const app = express();
-const __dirname = path.resolve();
-
-// Middlewares
-app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(cookieParser());
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/post", postRoutes);
-app.use("/api/comment", commentRoutes);
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-// Handle other requests by serving index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  const status = err.statusCode || 500;
-  const mess = err.message || "internal server error";
-  res.status(status).json({ success: false, status, mess });
-});
-
-// Database connection
 mongoose
   .connect(process.env.DB)
   .then(() => {
@@ -50,7 +24,25 @@ mongoose
     console.log("not connected", err);
   });
 
-// Start server
+const __dirname = path.resolve();
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
+});
+app.use(cookieParser());
+app.use("/api/auth", userRoutes);
+app.use("/api/user", authRoutes);
+app.use("/api/post", postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const mess = err.message || "internal server error";
+  res.status(status).json({ success: false, status, mess });
 });
