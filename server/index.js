86247fbc5d901fc52cf90d8bb3629d93f06d715f
Piyush Kumar <kumarchspiyush@gmail.com>
2024-10-15 12:13:33 +0530

    needed

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();  // This should be at the top
+
 import express from "express";
 const app = express();
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from 'cors';
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js"; // authRoutes
-import postRoutes from "./routes/post.routes.js"
+import userRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import commentRoutes from './routes/comment.routes.js';
 import cookieParser from 'cookie-parser';
-app.use(express.json());
 import path from 'path';
 
-
-dotenv.config();
+app.use(express.json());
 // app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 mongoose
@@ -29,6 +29,7 @@ const __dirname = path.resolve();
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
 app.use(cookieParser());
 app.use("/api/auth", userRoutes);
 app.use("/api/user", authRoutes);
