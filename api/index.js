3c97b217e0399bddb9e1462bfc154b09fc6d8a6d
Piyush Kumar <kumarchspiyush@gmail.com>
2024-12-02 22:13:19 +0530

    changes

--- api/index.js
+++ api/index.js
@@ -12,23 +12,24 @@ import authRoutes from './routes/user.routes.js';
 import postRoutes from './routes/post.routes.js';
 import commentRoutes from './routes/comment.routes.js';
 
-const app = express();
-const __dirname = path.resolve();
 
-// Use CORS to allow cross-origin requests from your deployed frontend
-app.use(cors({
-  origin: 'https://personal-blog-bo87.onrender.com', // Your deployment URL
-  credentials: true, // Allow cookies to be sent with requests
-}));
 
-app.use(express.json());
-app.use(cookieParser());
+
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB)
   .then(() => console.log("db connected"))
   .catch(err => console.log("not connected", err));
-
+  
+const __dirname = path.resolve();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+// Start server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 // Define API routes
 app.use('/api/auth', userRoutes);
 app.use('/api/user', authRoutes);
@@ -45,12 +46,13 @@ app.get('*', (req, res) => {
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
-  const status = err.statusCode || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal server error';
-  res.status(status).json({ success: false, status, message });
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
-// Start server on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+
